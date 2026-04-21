b95f1f9f3609dd63bd58c531ccccd6650bf6a91d
Sickle41 <sickle41@gmail.com>
2026-04-21 12:26:32 -0500

    FirstFunction

---
+++ main.js
@@ -0,0 +1,7 @@
+/*
+    Define a function with a single parameter. When the function
+    is invoked, an argument is required.
+*/
+const hailTheQueen = (nameString) => {
+    return `Hail Her Majesty, ${nameString}.`  // This function returns a string
+}
