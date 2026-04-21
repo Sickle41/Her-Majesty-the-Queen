75996d4e75ef892f8a874dda7420c27dff36eaae
Sickle41 <dracopest@gmail.com>
2026-04-21 16:13:23 -0500

    DataModule

--- main.js
+++ main.js
@@ -1,33 +1,6 @@
-const tributeChest = []
+const {tributeChest, queens} = require("./data.js")
 
-const payTribute = (id, description, queenId) => {
-    const tributeObject = {
-        id: id,
-        description: description,
-        queenId: queenId
-    }
-    tributeChest.push(tributeObject)
-}
 
-payTribute(1, "Gold", 1)
-payTribute(2, "Incense", 2)
-payTribute(3, "Spices", 3)
-payTribute(4, "Silver", 4)
-const queens = []
-
-const createQueen = (queenId, queenName) => {
-    const queenObject = {
-        id:queenId, 
-        name: queenName
-    }
-
-    queens.push(queenObject)
-}
-
-createQueen(1, "Trinity Terry")
-createQueen(2, "Margaret Thatcher")
-createQueen(3, "Dianna Ross")
-createQueen(4, "Emily Prentiss")
 /*
     Define a function with a single parameter. When the function
     is invoked, an argument is required.
